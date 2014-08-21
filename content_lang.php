65c5921ebc345920c8ebd799927f2b8a9e169b1f
Diff Git Server <dilshad@difftechnology.com>
2014-08-21 12:40:57 +0530

    First

---
+++ content_lang.php
@@ -0,0 +1,35 @@
+<?php
+/******************************************
+US English
+Admin Content Language
+******************************************/
+
+$lang['content_collections']					= 'Content Collections';
+$lang['content_collection']					= 'Content Collection';
+$lang['contents']							= 'Contents';
+$lang['name']								= 'Name';
+$lang['no_content_collections']				= 'There are currently no content collections';
+$lang['add_new_content_collection']			= 'Add New Content Collection';
+$lang['content_collection_not_found']		= 'The requested content collection could not be found.';
+$lang['content_collection_form']				= 'Content Collection Form';
+$lang['message_content_collection_saved']	= 'The content collection has been saved!';
+$lang['message_delete_content_collection']	= 'The content collection has been deleted.';
+$lang['confirm_delete_content_collection']	= 'Are you sure you want to delete this content collection and all it\\\'s contents?';
+$lang['content_not_found']					= 'The requested content could not be found.';
+
+$lang['title']					= 'Title';
+$lang['link']					= 'Link';
+$lang['enable_date']			= 'Enable Date';
+$lang['disable_date']			= 'Disable Date';
+$lang['new_window']				= 'New Window';
+$lang['image']					= 'Image';
+$lang['current_file']			= 'Current File';
+$lang['confirm_delete_content']	= 'Are you sure you want to delete this content?';
+$lang['add_new_content']			= 'Add New Content';
+$lang['no_contents']				= 'There are currently no contents';
+$lang['message_content_saved']	= 'The content has been saved!';
+$lang['date_error']				= 'The "Disable On" date cannot come on or before the "Enable On" date.';
+$lang['content_form']			= 'Content Form';
+$lang['message_delete_content']	= 'The content has been deleted.';
+$lang['sort']					= 'Sort';
+$lang['sort_contents']			= 'Contents are sortable! Just drag and drop them in the order you would like for them to appear.';
---
+++ operator_lang.php
@@ -0,0 +1,36 @@
+<?php
+/******************************************
+US English
+operator Administrator Management Language
+******************************************/
+
+$lang['operators']					= 'Operators';
+$lang['admin_form']				= 'Administrator Form';
+$lang['add_new_admin']			= 'Add New operator';
+$lang['error_self_delete']		= 'You cannot delete yourself';
+$lang['admin_not_found']		= 'The requested administrator could not be found';
+$lang['firstname']				= 'First Name';
+$lang['lastname']				= 'Last Name';
+$lang['email']                  = 'Email';
+$lang['username']               = 'Username';
+$lang['access']					= 'Access';
+$lang['password']				= 'Password';
+$lang['confirm_password']		= 'Confirm Password';
+$lang['message_user_saved']		= 'The administrator has been saved!';
+$lang['message_operator_saved']	= 'The operator has been saved!';
+
+$lang['message_operator_deleted']	= 'The operator has been deleted.';
+$lang['message_user_deleted']	= 'The administrator has been deleted.';
+$lang['error_username_taken']	= 'The requested username is already in use.';
+$lang['information']			= 'Information';
+$lang['confirm_delete']			= 'Are you sure you want to delete this Operator?';
+$lang['title']					= 'Operator/Title';
+$lang['phone']					= 'Phone';
+$lang['email']					= 'Email';
+$lang['address']				= 'Address';
+$lang['status']					= 'Status';
+$lang['status_comments']					= 'Status Comments';
+
+
+
+
